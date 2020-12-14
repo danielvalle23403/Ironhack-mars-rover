10697dcec87970f56570c96c1bc208fab5794771
Daniel <dvalle23403@columbushs.com>
2020-12-14 16:33:27 -0500

    Iteration 1 and 2

--- javascripts/app.js
+++ javascripts/app.js
@@ -13,3 +13,29 @@ function turnRight(rover){
 function moveForward(rover){
   console.log("moveForward was called")
 }
+//  Iteration 1
+let rover = {
+  direction: "N"
+}
+
+// Iteration 2
+function turnLeft(rover){
+// Condition statements to check which direction its facing
+if (rover.direction == "N"){
+rover.direction = "W" ;
+console.log("Direction is W");
+} else if(rover.direction == "W"){
+    rover.direction = "S";
+    console.log("Direction is S");
+} else if(rover.direction == "S"){
+    rover.direction = "E"; 
+} else if(rover.direction == "E")
+    rover.direction = "N"; 
+}
+
+
+function turnRight(rover){
+
+}
+
+
