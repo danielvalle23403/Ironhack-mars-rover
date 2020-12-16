9230029ca585013e8737dea1d7f6305d35e56d60
Daniel <dvalle23403@columbushs.com>
2020-12-16 12:35:52 -0500

    Completed

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,20 +2,12 @@
 // ======================
 
 // ======================
-function turnLeft(rover){
-  console.log("turnLeft was called!");
-}
-
-function turnRight(rover){
-  console.log("turnRight was called!");
-}
-
-function moveForward(rover){
-  console.log("moveForward was called")
-}
 //  Iteration 1
 let rover = {
-  direction: "N"
+  direction: "N",
+  x: 0 ,
+  y: 0 ,
+  travelLog: []
 }
 
 // Iteration 2
@@ -23,19 +15,83 @@ function turnLeft(rover){
 // Condition statements to check which direction its facing
 if (rover.direction == "N"){
 rover.direction = "W" ;
-console.log("Direction is W");
+console.log("Direction is West");
 } else if(rover.direction == "W"){
     rover.direction = "S";
-    console.log("Direction is S");
+    console.log("Direction is South");
 } else if(rover.direction == "S"){
     rover.direction = "E"; 
-} else if(rover.direction == "E")
-    rover.direction = "N"; 
+    console.log("Direction is East");
+} else if(rover.direction == "E"){
+    rover.direction = "N";
+    console.log("Direction is North");}
 }
-
 
 function turnRight(rover){
-
+// Condition statements to check which direction its facing
+if (rover.direction == "N"){
+rover.direction = "E" ;
+console.log("Direction is East");
+} else if(rover.direction == "E"){
+    rover.direction = "S";
+    console.log("Direction is South");
+} else if(rover.direction == "S"){
+    rover.direction = "West"; 
+} else if(rover.direction == "W"){
+    rover.direction = "North";}
 }
+
+
+
+
+
+
+// Iteration 3
+function moveForward(rover){
+  // move north
+if(rover.direction === "N"){
+  rover.y--; 
+  console.log(`X: ${rover.x}, Y: ${rover.y}`);
+  // move south
+} else if(rover.direction === "S"){
+  rover.y++;
+console.log(`X: ${rover.x}, Y: ${rover.y}`);
+  // move east
+} else if(rover.direction === "E"){
+  rover.x++;
+  console.log(`X: ${rover.x}, Y: ${rover.y}`);
+  // move west
+} else if(rover.direction === "W"){
+  rover.x--;
+  console.log(`X: ${rover.x}, Y: ${rover.y}`);
+}
+}
+
+
+// Iteration 4
+
+
+function commands(steps) {
+  // i = 0 ->l -> turnLeft
+  // i = 1 ->l -> turnleft
+  // i = 2 -> f ->  forward
+  // i = 3 -> f -> forward
+  // i = 4 -> r -> turnright
+  // i = 5 -> r -> turnright
+for(let i = 0; i < steps.length; i++){
+  if(steps.charAt (i) === "r"){
+    turnRight(rover);
+  } else if(steps.charAt (i) === "l"){
+    turnLeft(rover);
+  } else if(steps.charAt (i) === "f"){
+    moveForward(rover);
+  }
+  //Iteration 5
+// push step
+rover.travelLog.push(steps.charAt(i));
+}
+}
+
+console.log(`Travel log ${rover.travelLog}`);
 
 
